331b3a8ebc6f20c22bf64c820e312fd9e867366a
Abby Kelly <akindred4@ivytech.edu>
2022-03-10 15:04:16 -0500

    heroku Deploy 2

--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,8 @@
 module.exports = {
   // usercontroller: require('./usercontroller'),
-  chorecontroller: require('./chorecontroller'),
+  // chorecontroller: require('./chorecontroller'),
   usercontroller: require("./usercontroller"),
   // roomcontroller: require('./roomcontroller'),
   roomcontroller: require("./roomcontroller"),
+  chorecontroller: require("./chorecontroller"),
 };
