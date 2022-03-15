9646689a74e4334f3a64e33056f6f4c8ec876dff
Abby Kelly <akindred4@ivytech.edu>
2022-03-15 13:49:22 -0400

    initial commit

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use(Express.json());
 
 const controllers = require('./controller/index')
 
-app.use('/user', controllers.userController);
+app.use('/user', controllers.usercontroller);
 app.use(middleware.validateSession);
-app.use('/room', controllers.roomController);
+app.use('/room', controllers.roomcontroller);
 app.use('/chore', controllers.chorecontroller);
 
 dbConnection.authenticate()
